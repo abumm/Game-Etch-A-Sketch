d988aa9fa600a4f2b8dbe6aefeb0934ee131636b
Abu <abu.mamooji@gmail.com>
2022-10-25 15:50:09 -0700

    insterted grid sizing funtion and error handling via button

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
+//globals
+let newXY = 0 ;
+//default grid size
+let x = 16; 
+
+
 //creating grid div 
 let grid = document.createElement('div');
 grid.className = 'grid';
 
-let main = document.querySelector('body');
+let main = document.querySelector('._container ');
 main.appendChild(grid);
+
+//create button to edit size of grid
+let gridSize = document.createElement('button');
+gridSize.innerText = 'Change Grid Size';
+gridSize.className = 'gridButton';
+main.appendChild(gridSize);
 
 
 //call grid function 
-gridCreate();
+gridCreate(x);
 
 //hover listner
 addEventListener('mouseover', (event) =>{
@@ -15,26 +27,49 @@ addEventListener('mouseover', (event) =>{
         event.target.style.backgroundColor = 'red';
     }
     else {
-
+        //do nothing; 
     }
 })
+//button listner    
+gridSize.addEventListener('click', function()
+ { sizeFunction();
+});
+
 
 //creating grid function 
-function gridCreate(){
+function gridCreate(x){
     /* let rows = document.createElement('div');
     rows.className = 'row';
     let columns = document.createElement('div');
     columns.className = 'column'; */
 
-    for (let r = 0; r < 16; r++){
+    for (let r = 0; r < x; r++){
         let rows = document.createElement('div');
         rows.className = 'row';
-        for (let c = 0; c < 16; c++){
+        for (let c = 0; c < x; c++){
             let gridBox = document.createElement('div');
             gridBox.className = 'box';
             rows.appendChild(gridBox);
         }
         grid.appendChild(rows);
     }
-
 }
+
+function sizeFunction(){
+    newXY = prompt("Enter a number to resize your drawing board.");
+
+    if(isNaN(newXY)){
+        alert("That's not a number, try again.");
+        sizeFunction();
+    }
+    else{
+        grid.replaceChildren();
+        gridCreate(newXY);
+    }
+    }
+    console.log(newXY);
+
+    console.log(typeof(newXY));
+
+
+console.log(newXY);
